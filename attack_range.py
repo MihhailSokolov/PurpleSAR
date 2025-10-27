db1a8d63bd0b17a092b1bc0fc828e994eff97341
Marc Willaredt <marc.willaredt@kleeo.ch>
2025-10-27 17:17:20 +0100

    Fixed stop / resume commands for Azure machines

--- attack_range.py
+++ attack_range.py
@@ -90,12 +90,12 @@ def destroy(args):
 def stop(args):
     controller = init(args)
     instance_ids = [id.strip() for id in args.instance_ids.split(',')] if args.instance_ids else None
-    controller.stop(instance_ids)
+    controller.stop()
     
 def resume(args):
     controller = init(args)
     instance_ids = [id.strip() for id in args.instance_ids.split(',')] if args.instance_ids else None
-    controller.resume(instance_ids)
+    controller.resume()
 
 def packer(args):
     controller = init(args)
