4e8502d4471de9947ba69fc92c6ae840070f8bcc
Marc Willaredt <marc.willaredt@kleeo.ch>
2025-11-26 10:46:03 +0100

    Corrected VM stopping / deallocating logic.

--- modules/azure_service.py
+++ modules/azure_service.py
@@ -69,13 +69,13 @@ def change_instance_state(key_name, ar_name, new_state, log):
     if new_state == 'stopped':
         for instance in instances:
             if instance['vm_obj'].instance_view.statuses[1].display_status == "VM running":
-                async_vm_stop = compute_client.virtual_machines.begin_power_off("ar-rg-" + key_name + '-' + ar_name, instance['vm_obj'].name)
+                async_vm_stop = compute_client.virtual_machines.begin_deallocate("ar-rg-" + key_name + '-' + ar_name, instance['vm_obj'].name)
                 log.info('Successfully stopped instance ' + instance['vm_obj'].name + ' .')
 
     elif new_state == 'running':
         for instance in instances:
             if instance['vm_obj'].instance_view.statuses[1].display_status == "VM stopped":
-                async_vm_start = compute_client.virtual_machines.begin_deallocate("ar-rg-" + key_name + '-' + ar_name, instance['vm_obj'].name)
+                async_vm_start = compute_client.virtual_machines.begin_start("ar-rg-" + key_name + '-' + ar_name, instance['vm_obj'].name)
                 log.info('Successfully started instance ' + instance['vm_obj'].name + ' .')
 
 
